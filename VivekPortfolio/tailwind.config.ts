eb9b5f8e580084dd2afd0bca75ecbf7ae38b1bff
vivek12320680 <viveknagar2005@gmail.com>
2026-03-22 17:17:08 +0530

    first commit

--- VivekPortfolio/tailwind.config.ts
+++ VivekPortfolio/tailwind.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
@@ -90,5 +91,5 @@ export default {
       },
     },
   },
-  // plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
